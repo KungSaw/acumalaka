36249166fb8846bd99553d6fccd0e42fe5a469aa
KungSaw <akungtlvl85@gmail.com>
2026-03-04 10:45:13 +0700

    Update cihuy.lua

--- cihuy.lua
+++ cihuy.lua
@@ -33,7 +33,7 @@ _G.FishItConfig = _G.FishItConfig or {
     ["Selling"] = {
         ["Auto Sell"] = true,
         ["Auto Sell Threshold"] = "Mythic",
-        ["Auto Sell Every"] = 50,
+        ["Auto Sell Every"] = 30,
     },
     ["Doing Quest"] = {
         ["Auto Ghostfinn Rod"] = true,
