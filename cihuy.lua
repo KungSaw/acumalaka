389b3129802377fa77e5a70e89b85cd13188bfdc
KungSaw <akungtlvl85@gmail.com>
2026-03-09 19:23:10 +0700

    Update cihuy.lua

--- cihuy.lua
+++ cihuy.lua
@@ -129,9 +129,9 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Endgame"] = "Treasure Room",
     },
 
-    ["ExtremeFpsBoost"] = false,
+    ["ExtremeFpsBoost"] = true,
     ["UltimatePerformance"] = false,
-    ["Disable3DRender"] = true,
+    ["Disable3DRender"] = false,
     ["AutoRemovePlayer"] = true,
 
     ["AutoReconnect"] = false,
