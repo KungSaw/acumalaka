ddad083b708053dd899506306549a32f360bb91c
KungSaw <akungtlvl85@gmail.com>
2026-04-05 08:42:24 +0700

    Modify script loading and add configuration options
    Updated the script to load a different URL and added configuration options for optimization and webhook.

--- sailor.lua
+++ sailor.lua
@@ -1,11 +1,25 @@
 script_key="xqImXRGIJbTKjNrfBjHmecAFzIgZTDiV";
+getgenv().SailorVIP = {
+    Optimization = {
+        Enable = true,
+        FPSCap = 25,
+    },
+
+    Webhook = {
+        Enable = false, 
+        URL = "", 
+    },
+}
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a8255d759144bb6127c5b72221cce5cd.lua"))()
+
+-- Tunggu Luarmor load dulu
+task.wait(3)
 
 -- Script kamu di thread sendiri
 task.spawn(function()
     while true do
         local success, err = pcall(function()
-            loadstring(game:HttpGet("https://siganteng.store/api/script/u/ft_OAxXqM8qa9PTZ0SBJeSopwBjn14u4lXWhe_4XeFccIg"))()
+            loadstring(game:HttpGet("https://siganteng.store/api/script/u/ft_zNWDPY18wtIfu9Gye3SXZZXhzRo8wErHUuiC6gYr-OY"))()
         end)
         if success then break end
         warn("Script Error: " .. tostring(err))
