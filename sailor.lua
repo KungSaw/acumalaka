7a9539629f5e3f493c32c5eff8e55cc858e554d6
KungSaw <akungtlvl85@gmail.com>
2026-04-08 14:22:39 +0700

    Refactor Sailor script with new configuration options

--- sailor.lua
+++ sailor.lua
@@ -1,31 +1,140 @@
-task.spawn(function()
- script_key="xqImXRGIJbTKjNrfBjHmecAFzIgZTDiV";
-getgenv().SailorVIP = {
-    Optimization = {
-        Enable = true, -- On/Off toàn bộ optimization script
-        FPSCap = 10, -- Số FPS cần lock (ví dụ: 10, 15, 30, 60)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/list/refs/heads/main/game"))()
+_G.FishItConfig = _G.FishItConfig or {
+    ["Fishing"] = {
+        ["Auto Perfect"] = false,
+        ["Random Result"] = true,
+
+        ["Auto Favorite"] = true,
+        ["Auto Unfavorite"] = false,
+        ["Fish Name"] = {
+            {Name = "Ruby", Variant = "Gemstone"},
+        },
+
+        ["Auto Accept Trade"] = true,
+        ["Auto Friend Request"] = false,
+    },
+    ["Auto Trade"] = {
+        ["Enabled"] = true,
+        ["Whitelist Username"] = {"mrrpacman"},
+        ["Category Fish"] = {
+            "Secret",
+        },
+        ["Fish Name"] = {
+            {Name = "Ruby", Variant = "Gemstone"},
+        },
+        ["Item Name"] = {
+            "Evolved Enchant Stone",
+        },
+    },
+    ["Farm Coin Only"] = {
+        ["Enabled"] = false,
+        ["Target"] = 190000,
+    },
+    ["Selling"] = {
+        ["Auto Sell"] = true,
+        ["Auto Sell Threshold"] = "Mythic",
+        ["Auto Sell Every"] = 50,
+    },
+    ["Doing Quest"] = {
+        ["Auto Ghostfinn Rod"] = true,
+        ["Auto Element Rod"] = false,
+        ["Auto Diamond Rod"] = false,
+        ["Unlock Ancient Ruin"] = false,
+        ["Allowed Sacrifice"] = {
+            "Ghost Shark",
+        },
+        ["FARM_LOC_SECRET_SACRIFICE"] = "Ocean",
+
+        ["Minimum Rod"] = "Astral Rod",
+    },
+    ["WebHook"] = {
+        ["Link Webhook"] = "https://discord.com/api/webhooks/1480867201194922077/DCUp7S3qkaA_lf5IfZMuLE3d4PqwcTHIjd_8HWPMktXfQN7uV2fzXm5VP0f0dFHmRQHy",
+        ["Auto Sending"] = true,
+        ["Category"] = {"Secret"},
+
+        ["Link Webhook Quest Complete"] = "https://discord.com/api/webhooks/1480867278240092235/kNig-JxTskemWG4AwdZwKdD666ZaBRcDeizltPMILfaYPcJatVZ_moNuf_cxFa2XscDg",
+    },
+    ["Weather"] = {
+        ["Auto Buying"] = true,
+        ["Minimum Rod"] = "Astral Rod",
+        ["Weather List"] = {
+            "Wind",
+            "Cloudy",
+            "Storm",
+        },
+    },
+    ["Potions"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Astral Rod",
+    },
+    ["Totems"] = {
+        ["Auto Use"] = true,
+        ["Minimum Rod"] = "Ghostfinn Rod",
+        ["Buy List"] = {
+            ["Mutation Totem"] = 10,
+        },
+    },
+    ["Event"] = {
+        ["Start Farm"] = false,
+        ["Minimum Rod"] = "Ghostfinn Rod",
+        ["Event List"] = {
+            "Megalodon Hunt",
+        },
+    },
+    ["Enchant"] = {
+        ["Auto Enchant"] = false,
+        ["Roll Enchant"] = false,
+        ["Evolved Roll Enchant"] = false,
+        ["Enchant List"] = {
+            "Cursed I",
+        },
+        ["Second Enchant"] = false,
+        ["Allowed Sacrifice"] = {
+            "Frostborn Shark",
+            "Cryoshade Glider",
+            "Giant Squid",
+            "Panther Eel",
+        },
+        ["Second Enchant List"] = {
+            "Cursed I",
+            "Perfection",
+            "Empowered I",
+        },
+        ["Minimum Rod"] = "Ghostfinn Rod",
+    },
+    ["Bait List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {
+            "Midnight Bait",
+            "Chroma Bait",
+            "Floral Bait",
+            "Singularity Bait",
+        },
+        ["Endgame"] = "Floral Bait",
+    },
+    ["Rod List"] = {
+        ["Auto Buying"] = true,
+        ["Buy List"] = {
+            "Grass Rod",
+            "Midnight Rod",
+            "Astral Rod",
+            "Ares Rod",
+        },
+        ["Location Rods"] = {
+            ["Fisherman Island"] = {"Starter Rod"},
+            ["Tropical Grove"] = {"Grass Rod", "Midnight Rod"},
+            ["Treasure Room"] = {"Ares Rod", "Astral Rod"},
+            ["Treasure Room"] = {"Element Rod", "Ghostfinn Rod"},
+        },
+        ["Endgame"] = "Treasure Room",
     },
 
-    Webhook = {
-        Enable = false, -- On/Off gửi webhook thông báo crate
-        URL = "", 
-    },
+    ["ExtremeFpsBoost"] = false,
+    ["UltimatePerformance"] = true,
+    ["Disable3DRender"] = true,
+    ["AutoRemovePlayer"] = true,
+
+    ["AutoReconnect"] = false,
+    ["HideGUI"] = false,
+    ["EXIT_MAP_IF_DISCONNECT"] = true,
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a8255d759144bb6127c5b72221cce5cd.lua"))()
-end)
-
--- Eksekusi Script 2 (Siganteng) dengan sistem Auto-Retry
-task.spawn(function()
-    while true do
-        local success, err = pcall(function()
-            loadstring(game:HttpGet("https://siganteng.store/api/script/u/ft_OAxXqM8qa9PTZ0SBJeSopwBjn14u4lXWhe_4XeFccIg"))()
-        end)
-
-        if success then 
-            break -- Kalau sukses, hentikan looping
-        end
-
-        warn("Script Siganteng gagal dimuat, mencoba lagi dalam 5 detik... Error: " .. tostring(err))
-        task.wait(5)
-    end
-end)
