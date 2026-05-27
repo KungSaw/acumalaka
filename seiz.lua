8e40da5fdd0cfd84b3eb322803ac9d7bf19a720e
KungSaw <akungtlvl85@gmail.com>
2026-05-28 04:35:33 +0700

    Update seiz.lua

--- seiz.lua
+++ seiz.lua
@@ -15,7 +15,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Auto Trade"] = {
         ["Enabled"] = true,
-        ["Whitelist Username"] = {"bontotevelyn", "graceneonmagic89"},
+        ["Whitelist Username"] = {"evelynclara22", "mrrpacman", "clarabelle730", "bontotevelyn", "ELxSaveSaw", "graceneonmagic89"},
         ["Category Fish"] = {
             "Secret",
         },
