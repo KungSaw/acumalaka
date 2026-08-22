ffdd63640a1cdcb67cde88e97d456c3a4903acdd
KungSaw <akungtlvl85@gmail.com>
2026-08-22 16:27:10 +0700

    Update sailor.lua

--- sailor.lua
+++ sailor.lua
@@ -1,176 +1,107 @@
-_G.FishItConfig = _G.FishItConfig or {
-    ["Fishing"] = {
-        ["Auto Perfect"] = false,
-        ["Random Result"] = false,
+getgenv().VO_CONFIG = {
+    DeviceName = "CRYSTALL",
 
-        ["Auto Favorite"] = true,
-        ["Auto Unfavorite"] = false,
-        ["Fish Name"] = {
-            "Sacred Guardian Squid",
-            {Name = "Ruby", Variant = "Gemstone"},
-            -- {Variant = "Leviathan Rage"}, -- Variant Only
-            -- {Tier = "Secret", Variant = "Leviathan Rage"},  -- Tier + Variant
-        },
+    -- === MAIN FARM (choose one mode) ===
+    PotFarm = true,
+    EggFarm = false,
+    PetFarm = true,
+    KeepEggFarm = false,
+    KeepPetFarm = true,
+    EggName = {"Egg Name"},
+    PetFarmList = {"Oakee", "Black Tiger", "Kakapo", "Mexican Wolf", "Black Rhino", "Pangolin", "Black-Footed Ferret", "Abyssinian Cat", "Ocelot", "Orangutan", "Parakeet", "Zebra", "Badger", "Corgi", "Ginger Cat", "Gold Mahi Mahi", "Grinmoire", "Orca", "Pomeranian", "Ribbon Seal", "Robot", "Starfish", "Swordfish", "Toucan"},
+    PrioritizePet = "2D Kitty",
 
-        ["Auto Accept Trade"] = true,
-        ["Auto Friend Request"] = true,
+    -- === PET PEN ===
+    PetPen = true,
+    CustomPenEggs = {"Endangered Egg", "Pet Egg", "Cracked Egg", "Starter Egg"},
+    CustomPenPets = {
+        "Abyssinian Cat", "Badger", "Black Rhino",
+        "Black-Footed Ferret", "Corgi",
+        "Ginger Cat", "Grinmoire", "Mexican Wolf",
+        "Oakee Knight", "Ocelot", "Orangutan", "Orca",
+        "Pangolin", "Parakeet", "Pomeranian", "Ribbon Seal",
+        "River Otter", "Robot", "Starfish", "Storm Condor", "Swordfish", "Toucan", "Zebra", "Oakee"
     },
-    ["Auto Trade"] = {
-        ["Enabled"] = true,
-        ["Whitelist Username"] ={"StormChaserRePlayzD1", "RuneqePhoenixg146496", "Xz_HuntergeRocket6eC", "H0ll0w8MMagicj1878", "VortexHgZeroG126794", "SwitchpvZoomV1792", "XzRiftdAOblivion9zX1", "LegendKcClawG1140", "KingTVBlad3t1958", "XzByte7XBladeOzX1961"},
-        ["Category Fish"] = {
-            "Secret",
-            -- {Tier = "Mythic", Variant = "Stone"}, -- Tier + Variant
-        },
-        ["Fish Name"] = {
-           
-            {Name = "Ruby", Variant = "Gemstone"},
-            -- {Variant = "Leviathan Rage"}, -- Variant Only
-        },
-        ["Item Name"] = {
-            "Evolved Enchant Stone", "Runic Enchant Stone",
-        },
-    },
-    ["Farm Coin Only"] = {
-        ["Enabled"] = false, -- Farm coins only [ cant buy rod, bait, enchant, weather ]
-        ["Target"] = 190000,
-    },
-    ["Selling"] = {
-        ["Auto Sell"] = true,
-        ["Auto Sell Threshold"] = "Mythic",
-        ["Auto Sell Every"] = 100,
-    },
-    ["Doing Quest"] = {
-        ["Auto Ghostfinn Rod"] = true,
-        ["Auto Element Rod"] = true,
-        ["Auto Element Rod 2"] = false,
-        ["Auto Diamond Rod"] = false,
-        ["Unlock Ancient Ruin"] = true,
-        ["Allowed Sacrifice"] = {
-            "Gladiator Shark",
-            "Cryoshade Glider",
-            "Mosasaur Shark",
-            "Queen Crab",
-            "King Crab",
-            "Giant Squid",
-            "King Jelly",
+    PrioritizePetPenTypes = {"Egg"},
 
+    -- === PET RELEASER ===
+    PetReleaser = true,
+    ReleasePets = {},
+    ExcludeReleasePets = {
+        "Frostbite Bear",
+        "Dragonfruit Fox",
+        "Dango Penguins",
+        "Silverback Gorilla",
+        "Sushi Penguin",
+        "Velocirooster",
+        "2D Kitty",
+        "Rainbow Trout",
+        "Tealwood Monster",
+        "Oakee Wizard", 
+        "General Sheepdog",
+        "Chihuahua"
+    },
+    ReleaseTypes = {"Neon", "Mega"},
+    ReleaseRarities = {},
+    ExcludeRarities = {},
 
-        },
-        ["FARM_LOC_SECRET_SACRIFICE"] = "Treasure Room",
+    -- === AGE PETS ===
+    AgePets = true,
+    AgePetsNames = {
+        "Abyssinian Cat", "Badger", "Black Rhino",
+        "Black-Footed Ferret", "Corgi",
+        "Ginger Cat", "Grinmoire", "Mexican Wolf",
+        "Oakee Knight", "Ocelot", "Orangutan", "Orca",
+        "Pangolin", "Parakeet", "Pomeranian", "Ribbon Seal",
+        "River Otter", "Robot", "Starfish", "Storm Condor", "Swordfish", "Toucan", "Zebra"
+    },
+    AgePetsTypes = {"ALL"},
 
-        ["Minimum Rod"] = "Astral Rod",
-    },
-    ["WebHook"] = {
-        ["Auto Sending"] = true,
-        ["Category"] = {
-            "Secret",
-            {Name = "Ruby", Variant = "Gemstone"},
-        },
-        ["Item Name"] = {},
-        ["Link Webhook"] = "",
-        ["Link Webhook Quest Complete"] = "",
-    },
-    ["Weather"] = {
-        ["Auto Buying"] = true,
-        ["Minimum Rod"] = "Astral Rod",
-        ["Weather List"] = {
-            "Wind",
-            "Storm",
+    -- === AUTO FUSE ===
+    AutoFuse = true,
+    AutoFuseBlacklist = {},
 
-        },
-    },
-    ["Potions"] = {
-        ["Auto Use"] = true,
-        ["Minimum Rod"] = "Astral Rod",
-    },
-    ["Totems"] = {
-        ["Auto Use"] = true,
-        ["Minimum Rod"] = "Ghostfinn Rod",
-        ["Buy List"] = {
-            ["Luck Totem"] = 1000,
-            
-            
-        },
-    },
-    ["Event"] = {
-        ["Start Farm"] = false,
-        ["Minimum Rod"] = "Element Rod",
-        ["Event List"] = {
-            "Megalodon Hunt",
-           
-            
-            
-            
-        },
-    },
-    ["Enchant"] = {
-        ["Auto Enchant"] = true,
-        ["Roll Enchant"] = false,
-        ["Evolved Roll Enchant"] = true,
-        ["Farm Until Enchant"] = true,
-        ["Enchant After X Stone"] = 25,
-        ["Enchant List"] = {
-            "SECRET Hunter",
-            
-          
-           
-           
-            
-       
-        },
-        ["Second Enchant"] = false,
-        ["Allowed Sacrifice"] = {
-            "King Crab",
-            "Queen Crab",
+    -- === BUY PETS ===
+    BuyPets = true,
+    BuyPetName = {"Crystal Egg", "Tealwood Monster", "Endangered Egg"},
 
-        },
-        ["Second Enchant List"] = {
-            "Reeler l",
-            
-            
-        },
-        ["Minimum Rod"] = "Element Rod",
+    -- === BOXES ===
+    BuyBoxes = true,
+    BoxName = "2D Box",
+    OpenBoxes = true,
+
+    -- === LURE ===
+    BaitName = "Bait Name",
+
+    -- === AUTO TRADE ===
+    AutoTrade = false,
+ReceiverUsernames = {
+},
+    TradeItemList = {
+        pets = {"Crystal Egg"}
     },
-    ["Bait List"] = {
-        ["Auto Buying"] = true,
-        ["Buy List"] = {
-            "Midnight Bait",
-            "Corrupt Bait",
-            "Aether Bait",
-            "Singularity Bait",
-            
-        },
-        ["Endgame"] = "Singularity Bait",
-    },
-    ["Rod List"] = {
-        ["Auto Buying"] = true,
-        ["Buy List"] = {
-            "Grass Rod",
-            "Midnight Rod",
-            "Astral Rod",
-            "Ares Rod",
-            
-        },
-        ["Location Rods"] = {
-            ["Fisherman Island"] = {"Starter Rod"},
-            ["Tropical Grove"] = {"Grass Rod", "Midnight Rod", "Astral Rod"},
-            ["Underwater City"] = {"Ghostfinn Rod", "Element Rod", "Diamond Rod"},
-            
-        },
-        ["Endgame"] = "",
+    TradePetType = {"ALL"},
+
+    -- === CASH TRANSFER ===
+    CashTransfer = false,
+    TransferMethods = {"mannequin"},
+    TransferAccount = "",
+
+    -- === DISCORD WEBHOOK ===
+    WebhookEnabled = true,
+    WebhookURL = "https://discord.com/api/webhooks/1422425905464348772/F-Fgz7wWxWP-qk1-c3T4z-N3ePkyEOJ2DhJE-7DDBukhIDokgtI8jqcbILUURveTVPGa",
+    WebhookPets = {
+        "Dragonfruit Fox",
+        "Dango Penguins",
+        "Silverback Gorilla",
+        "Velocirooster",
+        "Sushi Penguin",
+        "Tealwood Monster",
+        "Emberlight",
+        "Rainbow Trout"
     },
 
-    ["ExtremeFpsBoost"] = false,
-    ["UltimatePerformance"] = false,
-    ["Disable3DRender"] = false,
-    ["AutoRemovePlayer"] = true,
-
-    ["AutoReconnect"] = false,
-    ["HideGUI"] = false,
-    ["EXIT_MAP_IF_DISCONNECT"] = false,
+    ExtraOpti = false
 }
 
-script_key="CBF8AE2FFFE0CD4FA0E654BA31B72774";
-
-local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/voltrex2/VoHub/refs/heads/main/FARM"))()
