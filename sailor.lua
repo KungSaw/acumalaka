64a640dd8197dce7aaad0732a2cfac72dc4c49e0
KungSaw <akungtlvl85@gmail.com>
2026-08-30 09:08:12 +0700

    Update sailor.lua

--- sailor.lua
+++ sailor.lua
@@ -10,7 +10,7 @@ getgenv().VO_CONFIG = {
     KeepEggFarm = false,
     KeepPetFarm = true,
     EggName = {"Egg Name"},
-    PetFarmList = {"Budgie Witch", "Princess Mare", "Clumpty", "Granny Wolf", "Crimson Cape", "Little Lamb", "Oakee", "Black Tiger", "Camel", "Donkey", "Kakapo", "Mahi Mahi", "Muskrat", "Poodle", "Shih Tzu", "Mexican Wolf", "Black Rhino", "Pangolin", "Black-Footed Ferret", "Abyssinian Cat", "Ocelot", "Orangutan", "Parakeet", "Zebra", "Badger", "Corgi", "Ginger Cat", "Gold Mahi Mahi", "Grinmoire", "Orca", "Pomeranian", "Ribbon Seal", "Robot", "Starfish", "Swordfish", "Toucan"},
+    PetFarmList = {"California Condor", "Galapagos Sea Lion", "Budgie Witch", "Princess Mare", "Clumpty", "Granny Wolf", "Crimson Cape", "Little Lamb", "Oakee", "Black Tiger", "Camel", "Donkey", "Kakapo", "Mahi Mahi", "Muskrat", "Poodle", "Shih Tzu", "Mexican Wolf", "Black Rhino", "Pangolin", "Black-Footed Ferret", "Abyssinian Cat", "Ocelot", "Orangutan", "Parakeet", "Zebra", "Badger", "Corgi", "Ginger Cat", "Gold Mahi Mahi", "Grinmoire", "Orca", "Pomeranian", "Ribbon Seal", "Robot", "Starfish", "Swordfish", "Toucan"},
     PrioritizePet = "2D Kitty",
 
     -- === PET PEN ===
@@ -18,8 +18,8 @@ getgenv().VO_CONFIG = {
     CustomPenEggs = {"Fairytale Egg", "Cracked Egg", "Starter Egg", "Pet Egg"},
     CustomPenPets = {
         "Budgie Witch", "Princess Mare", "Clumpty", "Granny Wolf", "Crimson Cape", "Little Lamb",
-        "Abyssinian Cat", "Badger", "Black Rhino",
-        "Black-Footed Ferret", "Corgi",
+        "Galapagos Sea Lion", "Abyssinian Cat", "Badger", "Black Rhino",
+        "California Condor", "Black-Footed Ferret", "Corgi",
         "Ginger Cat", "Grinmoire", "Mexican Wolf",
         "Oakee Knight", "Ocelot", "Orangutan", "Orca",
         "Pangolin", "Parakeet", "Pomeranian", "Ribbon Seal",
@@ -59,8 +59,8 @@ getgenv().VO_CONFIG = {
     AgePets = true,
     AgePetsNames = {
         "Budgie Witch", "Princess Mare", "Clumpty", "Granny Wolf", "Crimson Cape", "Little Lamb",
-        "Abyssinian Cat", "Badger", "Black Rhino",
-        "Black-Footed Ferret", "Corgi",
+        "Galapagos Sea Lion", "Abyssinian Cat", "Badger", "Black Rhino",
+        "California Condor", "Black-Footed Ferret", "Corgi",
         "Ginger Cat", "Grinmoire", "Mexican Wolf",
         "Oakee Knight", "Ocelot", "Orangutan", "Orca",
         "Pangolin", "Parakeet", "Pomeranian", "Ribbon Seal",
