042cd9c0cc1f04abd483514db558586030f14753
KungSaw <akungtlvl85@gmail.com>
2026-09-12 07:33:13 +0700

    Update seiz.lua

--- seiz.lua
+++ seiz.lua
@@ -2,22 +2,15 @@ getgenv().ATConfig = {
     -- ## General
     HubKey = "cN971JQ7GqrYR_pdP3yP-QGgsWhTISQh0Ns0XK9Rr5c",
     MainAccUsername = {
-        "kurikan99",
-        "kurikan696",
-        "kurikan86",
-        "kurikan4",
-        "kurikan28",
-        "kurikan5",
-        "kurikan92",
-        "kurikan95",
-        "kurikan27",
-        "kurikan32",
-        "kurikan75",
-        "kurikan79",
-        "kurikan_0",
-        "kurikan_05",
-        "gemetar32",
-        "gemetar551"
+        "Harith289x",
+        "Julian378x",
+        "Kagura1276x",
+        "Kimmy126x",
+        "Nana89x1",
+        "parameter927",
+        "parameter130",
+        "parameter133",
+        "parameter401"
     },
     PetsToTrade = {"Crystal Egg", "2D Doggy", "2D Kitty", "Dragonfruit Fox", "Velocirooster", "Chihuahua", "Strawberry Tortle", "Three Blind Mice", "Purrowl", "Sushi Penguin", "Blue Whale", "Sea Turtle", "Dango Penguins", "Alicorn", "Diamond Dragon", "Diamond Unicorn", "Golden Dragon", "Golden Unicorn"},
     TradePetType = {"ALL"}, -- Choose any combination: "ALL", "Mega", "Neon", "Regular", "Neon_FG", "Regular_FG"
